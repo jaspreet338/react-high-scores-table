d5106d86087af485bfe6eb90e4f659095122dd09
jaspreet kaur <kaurjas1928@yahoo.com>
2022-11-19 12:58:00 +0000

    First solution

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import React from "react";
+import HighScoreTable from './HighScoreTable';
+import allCountryScores from "./scores.js";
+import './App.css';
+
+function App() {
+  return (
+  <HighScoreTable scores={allCountryScores} />
+  );
+}
+
+export default App;
